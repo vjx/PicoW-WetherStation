1e573e26790ea60ab35f070df3f2437901f407eb
Xis <84106516+Fmvmedeiros@users.noreply.github.com>
2024-11-21 16:00:01 +0000

    add info

--- iot_credentials.py
+++ iot_credentials.py
@@ -1,4 +1,4 @@
-THINGSPEAK_WRITE_API_KEY = 'MYAPIKEY' #Change to your API KEY
+THINGSPEAK_WRITE_API_KEY = 'MYAPIKEY' #Change to your Thingspeak API KEY
 
 ssid = 'MYSSID'     #Change to your Wifi SSID
 password = 'MYWIFIPASSWORD' #Change to your Wifi password
